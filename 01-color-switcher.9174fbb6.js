47a309b4eb6f72020b635bb636d1fffd368b5f5c
WOWmasterJulia <96340846+WOWmasterJulia@users.noreply.github.com>
2023-04-11 15:11:43 +0000

    Deploying to gh-pages from @ WOWmasterJulia/GOIT-JS-hw-09@fa351504722a2f50b7cfaa80c635692dd94a5d49 🚀

---
+++ 01-color-switcher.9174fbb6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=(document.querySelector("button[data-stop]"),document.querySelector("body"));console.log(t),t.addEventListener("click",(function(t){var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));console.log(e),o.style.backgroundColor=e}))}();
+//# sourceMappingURL=01-color-switcher.9174fbb6.js.map
