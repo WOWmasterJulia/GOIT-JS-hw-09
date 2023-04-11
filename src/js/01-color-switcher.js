fa351504722a2f50b7cfaa80c635692dd94a5d49
WOWmasterJulia <topazzz@ukr.net>
2023-04-11 18:11:16 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const startbtnEl = document.querySelector('button[data-start]');
+const stopbtnEl = document.querySelector("button[data-stop]");
+const bodyEl = document.querySelector("body");
+console.log(startbtnEl);
+startbtnEl.addEventListener("click", changeColor);
+// stopbtnEl.addEventListener("click", );
 
+function changeColor (evt) {
+    const color = getRandomHexColor();
+    console.log(color);
+  bodyEl.style.backgroundColor = color;
+};
