b40964e004ec9c5bb2490f1ea35445bedf0c2cc1
WOWmasterJulia <topazzz@ukr.net>
2023-04-11 21:07:09 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,6 @@
+// https://bool.dev/blog/detail/obyasnenie-event-loop-v-javascript-s-pomoshchyu-vizualizatsii
+// https://www.jscamp.app/ru/docs/javascript25/
+// https://uk.wikipedia.org/wiki/%D0%A7%D0%B0%D1%81_Unix
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
@@ -5,11 +8,19 @@ const startbtnEl = document.querySelector('button[data-start]');
 const stopbtnEl = document.querySelector("button[data-stop]");
 const bodyEl = document.querySelector("body");
 console.log(startbtnEl);
-startbtnEl.addEventListener("click", changeColor);
-// stopbtnEl.addEventListener("click", );
+let timerId = null;
+stopbtnEl.setAttribute('disabled', 'true');
 
-function changeColor (evt) {
-    const color = getRandomHexColor();
-    console.log(color);
-  bodyEl.style.backgroundColor = color;
+startbtnEl.addEventListener("click", changeColor);
+function changeColor() { 
+  
+  timerId = setInterval(() => { bodyEl.style.backgroundColor = getRandomHexColor() }, 1000);
+  startbtnEl.setAttribute('disabled', 'true');
+  stopbtnEl.removeAttribute('disabled');
 };
+
+stopbtnEl.addEventListener("click", () => {
+  clearInterval(timerId);
+  startbtnEl.removeAttribute('disabled');
+  stopbtnEl.setAttribute('disabled', 'true');
+});
