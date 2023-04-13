f4751f8ed5bc902291d4f45483ef3122acfa2ab9
WOWmasterJulia <topazzz@ukr.net>
2023-04-13 20:12:19 +0300

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -22,10 +22,10 @@ function createPromise(position, delay) {
 //   .then(value => console.log(value)) 
 //   .catch(error => console.log(error))
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
