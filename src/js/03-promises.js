35bdb096c266e387d2b5436a731ebc39132454cb
WOWmasterJulia <topazzz@ukr.net>
2023-04-14 19:31:51 +0300

    9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,53 +1,54 @@
 import Notiflix from 'notiflix';
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
-// const formEl = document.querySelector('.form');
-// // const  input= document.querySelector('.form delay');
-
-// const createPromise = (position, delay) => {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-  
-//     if (shouldResolve) {
-//       resolve("Yes");
-//     } else {
-//       reject("No");
-//     }
-//     return
-//   });
-// }
-
-// createPromise()
-//   .then(value => console.log(value))
-//   .catch(error => console.log(error));
-
-
-
-function El(first, delay, amount) {
-return new Promise((resolve, reject) => {
-const param = createPromise(2, 1500)
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '130px',
+  opacity: 1,
+  // ...
 });
-}
-El(3, 500, 4)
-  .then(value => console.log(value))
-  .catch(error => console.log(error));
 
+
+
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', genPromise);
+
+function genPromise(evt) {
+  evt.preventDefault();
+  let delay = Number(evt.currentTarget.elements.delay.value);
+  const step = Number(evt.currentTarget.elements.step.value);
+  const amount = Number(evt.currentTarget.elements.amount.value);
+  
+  for (let i = 1; i <= amount; i += 1) {
+    // 
+    
+    // Надо вызывать ф-ю createPromise:
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delay = delay + step;
+  }
+  // evt.currentTarget.reset();
+  // console.log(delay, step, amount);
+}
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
