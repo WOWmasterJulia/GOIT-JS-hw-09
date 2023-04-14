cc900e31e2702c13e2437d5960a39f7fe36b4ee1
WOWmasterJulia <topazzz@ukr.net>
2023-04-14 11:55:38 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,5 @@
 import Notiflix from 'notiflix';
+
 // function createPromise(position, delay) {
 //   const shouldResolve = Math.random() > 0.3;
 //   if (shouldResolve) {
@@ -8,19 +9,40 @@ import Notiflix from 'notiflix';
 //   }
 // }
 
+// const formEl = document.querySelector('.form');
+// // const  input= document.querySelector('.form delay');
+
+// const createPromise = (position, delay) => {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+  
+//     if (shouldResolve) {
+//       resolve("Yes");
+//     } else {
+//       reject("No");
+//     }
+//     return
+//   });
+// }
+
+// createPromise()
+//   .then(value => console.log(value))
+//   .catch(error => console.log(error));
+
+
+
+function El(first, delay, amount) {
+return new Promise((resolve, reject) => {
+const param = createPromise(2, 1500)
+});
+}
+El(3, 500, 4)
+  .then(value => console.log(value))
+  .catch(error => console.log(error));
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  
-  if (shouldResolve) {
-    resolve("Yes");
-  } else {
-    reject("No");
-  }
 }
-
-// promise
-//   .then(value => console.log(value)) 
-//   .catch(error => console.log(error))
 
 // createPromise(2, 1500)
 //   .then(({ position, delay }) => {
